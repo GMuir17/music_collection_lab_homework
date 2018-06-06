7c6403c73b9685b2d96b8c7fdb15f83ebfe6da44
helenosheaa <helenosheaa@gmail.com>
2018-06-06 15:48:06 +0100

    edit artist name passed

--- models/artist.rb
+++ models/artist.rb
@@ -29,6 +29,13 @@ class Artist
     return albums.map {|album| Album.new(album)}
   end
 
+  def update()
+    sql = "UPDATE artists SET name
+    = $1 WHERE id = $2"
+    values = [@name, @id]
+    SqlRunner.run(sql, values)
+  end
+
   def self.all()
     sql = "SELECT * FROM artists;"
     artists = SqlRunner.run(sql)
