c2e0a8d3b7c571a8b2b54a57a0a901c6ab35e432
helenosheaa <helenosheaa@gmail.com>
2018-06-06 15:30:20 +0100

    save methods added and working

---
+++ db/console.rb
@@ -0,0 +1,19 @@
+require('pry')
+require_relative('../models/album.rb')
+require_relative('../models/artist.rb')
+
+artist1 = Artist.new({
+  'name' => 'Iron Maiden'
+  })
+artist1.save()
+
+album1 = Album.new({
+  'title' => 'Powerslave',
+  'genre' => 'metal',
+  'artist_id' => artist1.id()
+  })
+album1.save()
+
+
+binding.pry
+nil
