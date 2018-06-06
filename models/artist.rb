5202380bf79aafd5fd9f34fff4e03755e2547977
helenosheaa <helenosheaa@gmail.com>
2018-06-06 15:34:05 +0100

    list all albums and artists passed

--- models/artist.rb
+++ models/artist.rb
@@ -6,19 +6,28 @@ class Artist
   attr_reader(:id)
   attr_accessor(:name)
 
-def initialize(options)
-  @id = options['id'].to_i() if options['id']
-  @name = options['name']
-end
+  def initialize(options)
+    @id = options['id'].to_i() if options['id']
+    @name = options['name']
+  end
 
-def save()
-  sql = "INSERT INTO artists (
-    name)
-    VALUES (
-    $1)
-    RETURNING id;"
-  values = [@name]
-  results = SqlRunner.run(sql, values)
-  @id = results[0]['id'].to_i()
-end
+  def save()
+    sql = "INSERT INTO artists (
+      name)
+      VALUES (
+        $1)
+        RETURNING id;"
+    values = [@name]
+    results = SqlRunner.run(sql, values)
+    @id = results[0]['id'].to_i()
+  end
+
+  def self.all()
+    sql = "SELECT * FROM artists;"
+    artists = SqlRunner.run(sql)
+    return artists.map {|artist| Artist.new(artist)}
+  end
+
+
+
 end
